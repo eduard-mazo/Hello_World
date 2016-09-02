cb8265cf3a8c2a412e4859f6173e7759c91bbe87
eduard-mazo <eduard.mazo@gmail.com>
2016-09-01 23:47:43 -0500

    Trabajo finalizado

--- Lab1_Problemas/Problemas.cpp
+++ Lab1_Problemas/Problemas.cpp
@@ -7,8 +7,9 @@ int elevar(int, int);
 int collatz(int, int *);
 
 long int x;
-int temp,temp2,a,b,c,d,e,n,i,j,y,z,w,count,count2,min1,hor1,min2,hor2,ptr=0;
+int temp,p,temp2,a,b,c,d,e,n,i,j,y,z,w,count,count2,min1,hor1,min2,hor2,ptr=0;
 int ja,ia,l,mov1,mov2,dim,k,dir,sum=0;
+float flota=0;
 int arra[10]={50000,20000,10000,5000,2000,1000,500,200,100,50};
 int arra2[20];
 int arra3[20];
@@ -20,10 +21,10 @@ int main()
 {cout<<"SELECCIONE EJERCICIOS "<<endl<<"-----------------"<<endl<<"#0  -- PROBLEMA #2"<<endl<<"#1  -- PROBLEMA #3"<<endl;
     cout<<"#2  -- PROBLEMA #4"<<endl<<"#3  -- PROBLEMA #5"<<endl<<"#4  -- PROBLEMA #6"<<endl;
     cout<<"#5  -- PROBLEMA #7"<<endl<<"#6  -- PROBLEMA #8"<<endl<<"#7  -- PROBLEMA #9"<<endl;
-    cout<<"#8  -- PROBLEMA #10"<<endl<<"#9 -- PROBLEMA #14"<<endl<<"#10 -- PROBLEMA #16"<<endl<<"#11 -- PROBLEMA #13"<<endl;
-    cout<<"#12  -- PROBLEMA #15"<<endl;
+    cout<<"#8  -- PROBLEMA #10"<<endl<<"#9  -- PROBLEMA #14"<<endl<<"#10 -- PROBLEMA #16"<<endl<<"#11 -- PROBLEMA #13"<<endl;
+    cout<<"#12 -- PROBLEMA #15"<<endl<<"#13 -- PROBLEMA #17"<<endl;
     cin>>temp;
-    
+
     switch(temp){
     case 0:
         cout<<"PROBLEMA #2"<<endl;
@@ -36,17 +37,17 @@ int main()
                 i--;
                 //cin.ignore().get(); //Pause Command for Linux Terminal
             }
-            
+
         }
-        
+
         for(i=0;i<=9;i++){
             cout<<arra[i]<<"-->"<<arra2[i]<<endl;// imprimo billetes y cantidad usada
-            
+
         }
         cout<<"LO QUE RESTA: "<<a<<endl;
-        
+
         break;
-        
+
     case 1:
         cout<<"PROBLEMA #3"<<endl;
         cout<<"INGRESE UN MES: ";
@@ -62,36 +63,36 @@ int main()
             {if(b>30)cout<<"FECHA INVALIDA "<<b<<"/"<<a<<endl;//Condicion para Febrero, bisiesto o no!
                 if(b==29)cout<<"POSIBLE BISIESTO "<<b<<"/"<<a<<endl;
                 else  cout<<"FECHA VALIDA "<<b<<"/"<<a<<endl;
-                
+
             }
-            
+
             if((a==4)||(a==6)||(a==9)||(a==11)){ // Condicion para meses de 30 dias
                 if(b>30)cout<<"FECHA INVALIDA "<<b<<"/"<<a<<endl;
                 else cout<<"FECHA VALIDA "<<b<<"/"<<a<<endl;
             }
-            
+
         }else cout<<"FECHA INVALIDA "<<b<<"/"<<a<<endl;
-        
+
         break;
-        
+
     case 2:
         cout<<"PROBLEMA #4"<<endl;
         cout<<"HORA 1: ";
         cin>>a;
         if(a<999){count++;
             d=1;}
-        
+
         while(a!=0)//Con el while obtenemos las unidades y podemos separa en cifras
         {
             c=a;
             a /= 10;
-            
+
             c%=10;
             if(d)arra2[count-1]=c;
             else arra2[count]=c;
             count++;
         }
-        
+
         cout<<endl<<"HORA 2: ";
         cin>>b;
         c=0;
@@ -153,65 +154,65 @@ int main()
             {
                 min1=n;
             }
-            
+
             y=hor1+hor2;
-            
+
             if(y<24) // Siempre y cuando tenga una hora menor a 24
             {
                 cout<<"LA HORA ES: "<<y<<":"<<min1<<endl;
                 if(z!=0)y+=z;
             }
             else cout<<endl<<"HORA INCORRECTA"<<endl;
-            
-            
+
+
         }
         else cout<<endl<<"HORA INCORRECTA"<<endl;
-        
-        
+
+
         break;
     case 3:
-        
+
         cout<<"PROBLEMA #5"<<endl;
         cout<<"PIRAMIDE"<<endl;
-        
+
         cout<<"INGRESE UN # IMPAR: ";
         cin>>a;
         if((a%2)==0)cout<<endl<<"NUMERO IVALIDO (PAR)"<<endl;
         else {cout<<endl<<"NUMERO CORRECTO (IMPAR)"<<endl<<endl;
-            
+
             a/=2;
-            
+
             for (int i = 0; i <=a; i++) {//Ciclo for que imprime parte superior de la piramide
-                
+
                 for (int j = 0; j <(a-i); j++) {//rellena espacios en blanco para ubicar el puntero
                     cout << ' ';
-                    
+
                 }
-                
+
                 for (int j = (a-i); j<=a+i ; j++) {//desde la ubicacion del puntero ingresa * para darle forma a la piramide
                     cout << "*";
                 }
-                
-                
+
+
                 cout << '\n';
             }
             for (int i = a-1; i >=0; i--) {// ciclo for que hace lo mismo que el anterior solo que en forma inversa
-                
+
                 for (int j = 0; j <(a-i); j++) {
                     cout << ' ';
-                    
+
                 }
-                
+
                 for (int j = (a-i); j<=a+i ; j++) {
                     cout << "*";
                 }
-                
-                
+
+
                 cout<<endl;
             }
         }
         break;
-        
+
     case 4:
         cout<<"PROBLEMA #6"<<endl;
         cout<<"NUMERO DE EULER"<<endl;
@@ -223,7 +224,7 @@ int main()
         x+=1;//Sumo 1 que corresponde a 1/0!
         cout<<endl<<"NUMERO DE EULER APROX: "<<x<<endl;
         break;
-        
+
     case 5:
         cout<<"PROBLEMA #7"<<endl;
         cout<<"FIBONACCI   "<<endl;
@@ -231,7 +232,7 @@ int main()
         cin>>a;
         arra5[0]=1;
         arra5[1]=1;
-        
+
         for(x=1;x<=a;x++)// Ciclo For para encontrar la susecion de Fibonacci
         {
             arra5[x+1]=arra5[x-1]+arra5[x];
@@ -240,15 +241,15 @@ int main()
                 if((arra5[x+1]%2)==0)// De la lista de Fibonacci encontrada descarto los que nos son PARES
                 {
                     j=j+arra5[x+1];//Variable para sumar los pares
-                    
+
                     ;}
             }
             cout<<endl<<" "<<arra5[x+1];// Imprimo lista Fibonacci
         }
         cout<<endl<<"LA SUMA DE LOS PARES MENORES QUE N ES: "<<j<<endl;
-        
+
         break;
-        
+
     case 6:
         cout<<"PROBLEMA #8"<<endl;
         cout<<"SUMA DE MULTIPLOS A y B MENORES QUE C "<<endl;
@@ -259,7 +260,7 @@ int main()
         cout<<endl<<"INGRESE C: ";
         cin>>c;
         n=1;
-        
+
         while(a*n<c)//Encuentro multiplos de A
         {
             arra2[n]=a*n;
@@ -271,33 +272,33 @@ int main()
             arra3[e]=b*e;
             e++;
         }
-        
+
         if(n>e)j=n; //Cual de los dos tiene mayor cantidad de elementos
         else j=e;
-        
+
         for(i=1;i<j;i++) // Ciclo For para buscar multiplos comunos y descartarlos
         {    arra4[i]=arra2[i];
-            
+
             for(d=1;d<j;d++){
-                
+
                 if(arra4[i]==arra3[d])arra3[d]=0;
-                
-                
+
+
             }
         }
         for(i=1;i<n;i++)// imprimo multimos de A con el formato respectivo
         {if(arra2[i]!=0)cout<<arra2[i]<<"+";
             x+=arra2[i];// Sumo multiplos A
         }
-        
+
         for(i=1;i<e;i++)// imprimo multimos de B con el formato respectivo
         {
             if(arra3[i]!=0)cout<<arra3[i]<<"+";
             x+=arra3[i];// Sumo multiplos B
         }
         cout<<'\b'; // Borro un + que me queda sobrando
-        
-        
+
+
         cout<<"="<<x<<endl; // Imprimo resultado de la suma
         break;
     case 7:
@@ -312,22 +313,22 @@ int main()
             ++count;
             temp%=10;
             arra5[count]=temp;
-            
+
         }
         //SUMO CADA NUMERO ELEVADO A SU RESPECTIVA POTENCIA
         for(i=count;i>0;i--)
         {
             y=y+elevar(arra5[i],arra5[i]);
-            
-            
+
+
         }
         //IMPRIMO EN PANTALLA
         cout<<"-------------------"<<endl;
         cout<<"---RESULTADO: "<<y<<"---"<<endl;
         cout<<"-------------------"<<endl;
-        
+
         break;
-        
+
     case 8:
         cout<<"PROBLEMA #10"<<endl;
         cout<<"ENESIMO PRIMO"<<endl;
@@ -340,17 +341,17 @@ int main()
                 if(c%i==0)
                     n++;
             }
-            
+
             if(n==2)a--;// Disminuye contador cuando es primo
             e=c;//alamceno el Primo hayado
             n=0;//Restauro contador
             c+=1; // Aumento contador para buscar proximo primo
             if(a==0)b=1;//termina programa cuando encuentra el Nsimo Primo
-            
+
         }
         cout<<endl<<"ENESIMO # PRIMO: "<<e<<endl;
         break;
-        
+
     case 9:
         cout<<"PROBLEMA #14"<<endl;
         cout<<"CALCULAR PALINDROMO MAS GRANDE"<<endl;
@@ -448,104 +449,132 @@ int main()
         }
         cout<<endl<<"SUMA DE NUMEROS PRIMOS HASTA "<<a<<" ES: "<<e<<endl;
         break;
+    case 13:
 
-    case 12:
-            cout<<"*** PROBLEMA 15 ***"<<endl;
-            cout<<"INGRESE UN NUMERO IMPAR, PARA GENERAR MATRIZ N*N: ";
-            cin>>dim;
-            k=dim;
-            k/=2;
-            if(k==0)
+        cout<<"PROBLEMA #17"<<endl;
+        cout<<"INGRESE UN NUMERO: ";
+        cin>>n;
+        flota=1;
+
+        while(!e)
+        {
+            p=flota;                     //Guardo numero a convertir
+            flota=flota+1;              //comienzo con la ec triangular n*(n+1)/2
+            flota/=2;
+            flota=flota*p;
+            j=flota;
+            flota=p;                    //recupero variable inicial
+            cout<<endl<<"NUMERO TRIANGULAR: "<<j<<endl;
+            for(i=1;i<=j;i++)// FOR para obetener divisores
             {
-                c=1;        // condicion que me permite verificar si el # es PAR o IMPAR
-                cout<<endl<<"INGRESASTE UN # PAR -.-"<<endl;
-            }
-            int Spiral[dim][dim]; // Determino el tamano que mi matriz va a tener
-            a=dim*dim;  // Inicia mi contador descendente, adquiere el valor maximo de la matriz n*n
-            ja=dim-1; // variable que me permite controlar el inicio en izq y el final en der de los ciclos for respectivos
-            d=ja; // variable que me permite controlar el inicio en up y el final en down de los ciclos for respectivos
-            mov2=d; // variable que controla el apuntador i en der y j en up
-            l=0; // variable que me permite controlar el inicio en izq y el final en der de los ciclos for respectivos
-            ia=1; // variable que me permite controlar el inicio en down y el final en up de los ciclos for respectivos
-            mov1=l; // apuntador i en izq y j en down
-            dir=0; // selector
-            sum+=a; // inicio la suma con el mayor valor
-            while(!c)
-            {
-                switch (dir) {
-                case 0: //derecha izquierda
-
-
-                    for(j=ja;j>=l;j--)
-                    {
-                        Spiral[mov1][j]=a;
-                        a--;
-                    }
-                    sum+=(a+1);// sumo la esquina superior izq
-                    dir=1;
-                    l++;
-                    break;
-                case 1: //arriba abajo
-
-                    for(i=ia;i<=d;i++)
-                    {
-                        Spiral[i][mov1]=a;
-                        a--;
-                    }
-                    sum+=(a+1); // sumo esquina inferior izq
-                    dir=2;
-                    d--;
-                    mov1++;
-                    break;
-                case 2: //izquierda derecha
-
-                    for(j=l;j<=ja;j++)
-                    {
-                        Spiral[mov2][j]=a;
-                        a--;
-                    }
-                    sum+=(a+1); // sumo esquina inferior der
-                    dir=3;
-                    ja--;
-                    break;
-
-                case 3: //abajo arriba
-
-                    for(i=d;i>=ia;i--)
-                    {
-                        Spiral[i][mov2]=a;
-                        a--;
-                    }
-                    sum+=(a); // sumo esquina superior der
-                    dir=0;
-                    ia++;
-                    mov2--;
-                    break;
-                case 4: // Case para Imprimir en pantalla
-
-                    cout<<"//--//--//--//--//--//--//--//--//--//--//--//--//--//--//"<<endl;
-                    cout<<"--//--//--//--//--//--//--//--//--//--//--//--//--//--//--"<<endl<<endl;
-                    for(i=0;i<dim;i++)
-                    {
-                        for(j=0;j<dim;j++)
-                        {   if(Spiral[i][j]<10)cout<<" "; // Condiciones para que la matriz se vea bonita
-                            if(Spiral[i][j]<100)cout<<" ";
-                            cout<<Spiral[i][j]<<"  ";// Imprime en pantalla los valores almacenados en la matriz
-                        }                            // de tal forma que se logre el efecto deseado.
-                        cout<<endl;
-                    }
-                    cout<<endl<<"LA SUMA DE LAS DIAGONALES ES: "<<(sum-1)<<endl;
-                    cout<<endl<<"//--//--//--//--//--//--//--//--//--//--//--//--//--//--//"<<endl;
-                    cout<<"--//--//--//--//--//--//--//--//--//--//--//--//--//--//--"<<endl;
-                    c=1; //exit while..
-                    break;
+                if(j%i==0)
+                {
+                    count++;// contador para chequear quien cumple con el objetivo
+                    cout<<i<<" ";
+                    if(count==n) e=1;
                 }
-                if(a<1)dir=4;
-
             }
-
+            flota++;
+            count=0;
+        }
+        cout<<endl<<"EL NUMERO TRIANGULAR CON MENOS DIVISORES QUE N ES: "<<j<<endl;
         break;
 
+
+    case 12:
+        cout<<"*** PROBLEMA 15 ***"<<endl;
+        cout<<"INGRESE UN NUMERO IMPAR, PARA GENERAR MATRIZ N*N: ";
+        cin>>dim; // dimension de la matriz
+        k=dim;
+        k/=2;
+        if(k==0)
+        {
+            c=1;        // condicion que me permite verificar si el # es PAR o IMPAR
+            cout<<endl<<"INGRESASTE UN # PAR -.-"<<endl;
+        }
+        int Spiral[dim][dim]; // Determino el tamano que mi matriz va a tener
+        a=dim*dim;  // Inicia mi contador descendente, adquiere el valor maximo de la matriz n*n
+        ja=dim-1; // variable que me permite controlar el inicio en izq y el final en der de los ciclos for respectivos
+        d=ja; // variable que me permite controlar el inicio en up y el final en down de los ciclos for respectivos
+        mov2=d; // variable que controla el apuntador i en der y j en up
+        l=0; // variable que me permite controlar el inicio en izq y el final en der de los ciclos for respectivos
+        ia=1; // variable que me permite controlar el inicio en down y el final en up de los ciclos for respectivos
+        mov1=l; // apuntador i en izq y j en down
+        dir=0; // selector
+        sum+=a; // inicio la suma con el mayor valor
+        while(!c)
+        {
+            switch (dir) {
+            case 0: //derecha izquierda
+
+
+                for(j=ja;j>=l;j--)
+                {
+                    Spiral[mov1][j]=a;
+                    a--;
+                }
+                sum+=(a+1);// sumo la esquina superior izq
+                dir=1;
+                l++;
+                break;
+            case 1: //arriba abajo
+
+                for(i=ia;i<=d;i++)
+                {
+                    Spiral[i][mov1]=a;
+                    a--;
+                }
+                sum+=(a+1); // sumo esquina inferior izq
+                dir=2;
+                d--;
+                mov1++;
+                break;
+            case 2: //izquierda derecha
+
+                for(j=l;j<=ja;j++)
+                {
+                    Spiral[mov2][j]=a;
+                    a--;
+                }
+                sum+=(a+1); // sumo esquina inferior der
+                dir=3;
+                ja--;
+                break;
+
+            case 3: //abajo arriba
+
+                for(i=d;i>=ia;i--)
+                {
+                    Spiral[i][mov2]=a;
+                    a--;
+                }
+                sum+=(a); // sumo esquina superior der
+                dir=0;
+                ia++;
+                mov2--;
+                break;
+            case 4: // Case para Imprimir en pantalla
+
+                cout<<"//--//--//--//--//--//--//--//--//--//--//--//--//--//--//"<<endl;
+                cout<<"--//--//--//--//--//--//--//--//--//--//--//--//--//--//--"<<endl<<endl;
+                for(i=0;i<dim;i++)
+                {
+                    for(j=0;j<dim;j++)
+                    {   if(Spiral[i][j]<10)cout<<" "; // Condiciones para que la matriz se vea bonita
+                        if(Spiral[i][j]<100)cout<<" ";
+                        cout<<Spiral[i][j]<<"  ";// Imprime en pantalla los valores almacenados en la matriz
+                    }                            // de tal forma que se logre el efecto deseado.
+                    cout<<endl;
+                }
+                cout<<endl<<"LA SUMA DE LAS DIAGONALES ES: "<<(sum-1)<<endl;
+                cout<<endl<<"//--//--//--//--//--//--//--//--//--//--//--//--//--//--//"<<endl;
+                cout<<"--//--//--//--//--//--//--//--//--//--//--//--//--//--//--"<<endl;
+                c=1; //exit while..
+                break;
+            }
+            if(a<1)dir=4;
+        }
+        break;
     }
 
 
