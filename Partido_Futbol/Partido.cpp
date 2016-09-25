f28f09ee71e9402e2422c19f23cdf1218281f159
eduard-mazo <eduard.mazo@gmail.com>
2016-09-25 11:59:01 -0500

    Correccion Random

--- Partido_Futbol/Partido.cpp
+++ Partido_Futbol/Partido.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
+#define clear() cout<<"\033[H\033[J"
 
 using namespace std;
 // srand(time(NULL)); SEMILLA PARA RAND;
@@ -45,15 +46,15 @@ public:
 int main()
 {
     Partido clasico;
- clasico.miPartido();
- cout<<"EL ENCUENTRO FINALIZO: ["<<clasico.Get_marcador()[0]<<","<<clasico.Get_marcador()[1]\
-    <<"]"<<endl;
+    clasico.miPartido();
+    cout<<"EL ENCUENTRO FINALIZO: ["<<clasico.Get_marcador()[0]<<","<<clasico.Get_marcador()[1]\
+       <<"]"<<endl;
 
 }
 void Partido::miPartido()
 {
     Set_values();
-    Set_team();
+    // Set_team();
     Set_marcador();
 
 }
@@ -87,41 +88,51 @@ int* Partido::Get_marcador()
 void Partido::Set_marcador()
 {
     srand(time(NULL));
+
     int temp=rand()%10;
-    int mientras=90;
-    int temp2,temp3,temp4=1;
+    int mientras=0;
+    int temp2=0,temp3=0,temp4=1;
     for(int i=0;i<temp;i++)
     {
         temp2=rand()%2;
-        temp3=rand()%11;
-        temp4=rand()%mientras;
+
+        temp4=rand()%20+mientras;
         mientras=temp4;
+        temp3=rand()%11;
 
         if(temp2)
         {
-            *(marcador+1)+=1;
             *(*(report+0)+i)/*[0][i]*/=temp2;// EQUIPO QUE METE GOL
-            report[1][i]=temp3;// JUGADOR QUE METE GOL
-            report[2][(temp-1)-i]=temp4;// MINUTO
+            *(*(report+1)+i)=temp3;// JUGADOR QUE METE GOL
+            *(*(report+2)+i)=temp4;// MINUTO
+            *(marcador+1)+=1;
+
         }
 
         else
         {
+            *(*(report+0)+i)/*[0][i]*/=temp2;// EQUIPO QUE METE GOL
+            *(*(report+1)+i)=temp3;// JUGADOR QUE METE GOL
+            *(*(report+2)+i)=temp4;// MINUTO
             *marcador+=1;
-            report[0][i]=temp2;// EQUIPO QUE METE GOL
-            report[1][i]=temp3;// JUGADOR QUE METE GOL
-            report[2][(temp-1)-i]=temp4;// MINUTO
+
         }
     }
+    //    for(int i=0;i<temp;i++){
+    //        *(*(report+0)+i)/*[0][i]*/=tempreport[0][i];// EQUIPO QUE METE GOL
+    //        *(*(report+1)+i)=tempreport[1][i];// JUGADOR QUE METE GOL
+    //        *(*(report+2)+i)=tempreport[2][i];// MINUTO
+    //    }
 
     Gol=temp;
 }
 
 void Partido::Set_team()
 {
+
     char temp[20];
     for(int i=0;i<2;i++)
-    {
+    {   clear();
         cout<<endl<<"INGRESE EL NOMBRE DEL EQUIPO ["<<i<<"]: ";
         cin>>temp;
         equipos[i].Set_name(temp);
@@ -159,8 +170,12 @@ void Equipo::Get_team_name(char* nombre)
 void Equipo::player_names()
 {
     char temp[20];
+    //    char temp2[10];
+    //    Get_team_name(temp2);
     for(int i=0;i<11;i++)
     {
+        clear();
+        cout<<"\t"<<nameE<<endl;
         cout<<endl<<"INGRESE EL NOMBRE DEL JUGADOR ["<<i+1<<"]: ";
         cin>>temp;
         jugadores[i].set_name(temp);
